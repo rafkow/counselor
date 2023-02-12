2b5682de5a3c44f001d3610c25959a8aaf0e468a
Rafał Kowalewski <rafalkowalewski@outlook.com>
2023-02-12 19:15:45 +0100

    config static files

--- counselor/settings.py
+++ counselor/settings.py
@@ -139,3 +139,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'register/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+DEFAULT_PAGE_NAME = 'Rejestr Kancelaria'
